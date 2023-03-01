96f81de2ead66b7799766d625899fc5759ee599b
felix <felix@eth.myguest.virtualbox.org>
2023-02-28 17:05:21 -0800

    Redeploy

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ import MessageBox from "./components/MessageBox"
 
 // The address of the contract deployed the Fuel testnet
 const CONTRACT_ID =
-  "0x8214e38080d1f46472963fbfb5f36c6c255339468dc7f495ba641a173fab3344";
+  "0xc48e025c3b129b3f2412d2d3165032c4ebc0fc19f20afa0e6b7cd6f28cee6ed4";
 
 //the private key from createWallet.js
 const WALLET_SECRET =
@@ -36,47 +36,17 @@ function App() {
 
   const [loading, setLoading] = useState(false);
   const [registered, setRegistered] = useState(false);
-  
-  // check if user registered
-  useEffect(() => {
-    async function main() {
-      // Executes the counter function to query the current contract state
-      // the `.get()` is read-only, because of this it don't expand coins.
-      const { value } = await contract.functions.is_registered().txParams({ gasPrice: 1 }).call();
-      console.log(value);
-      // const { value } = await contract.functions.is_registered().get();
-      // console.log(value);
-      // setRegistered(value);
-    }
-    main();
-  }, []);
 
-  // register user
-  async function register() {
-    // a loading state
-    setLoading(true);
-    // Creates a transactions to call the increment function
-    // because it creates a TX and updates the contract state this requires the wallet to have enough coins to cover the costs and also to sign the Transaction
-    try {
-
-      // await contract.functions.register(MSG_PK).txParams({ gasPrice: 10 }).call();
-      // await contract.functions.add_contact(wallet.address.toHexString()).txParams({ gasPrice: 10 }).call();
-      // await contract.functions.is_registered().txParams({ gasPrice: 1 }).call();
-      const { value } = await contract.functions.is_registered().txParams({ gasPrice: 10 }).call();
-      // await contract.functions.add_contact(wallet.address.toHexString()).txParams({ gasPrice: 10 }).call();
-      await contract.functions.message(wallet.address.toHexString(), "HELLO").txParams({ gasPrice: 10 }).call();
-      // console.log(value);
-      setRegistered(value);
-    } finally {
-      setLoading(false);
-    }
+  const messageBoxArgs = {
+    wallet: wallet,
+    contract: contract,
   }
   
 
   return (
     <div className="App">
       
-        <MessageBox wallet={wallet} contract={contract} />
+        <MessageBox {...messageBoxArgs} />
     </div>
   );
 }
--- src/components/MessageBox.tsx
+++ src/components/MessageBox.tsx
@@ -1,12 +1,19 @@
 import React, { useEffect, useState } from "react";
 import logo from './logo.svg';
-import './App.css';
+import '../App.css';
 import { Wallet, WalletUnlocked, bn, hexlify,Interface, Contract } from "fuels";
 // Import the contract factory -- you can find the name in index.ts.
 // You can also do command + space and the compiler will suggest the correct name.
 import { PostmanContractAbi__factory } from "../contracts/factories/PostmanContractAbi__factory";
+import { PostmanContractAbi } from "../contracts";
 
-const MessageBox = (wallet:  WalletUnlocked, contract: Contract) => {
+
+type MessageBox = {
+    wallet: WalletUnlocked,
+    contract: PostmanContractAbi
+};
+
+const MessageBox = ({wallet, contract}:MessageBox) => {
     const MSG_PK = '0x37de64bb3d416707d1833a361c60e0cec856d3bae534619646008abb51e48cb5';
     const MSG_SECRET = bn('0xed385ab156b48da8d72d423905a10d62662c0a3ebc65849254afa1c82d532c39');
 
@@ -15,18 +22,18 @@ const MessageBox = (wallet:  WalletUnlocked, contract: Contract) => {
 
 
     // check if user registered
-    useEffect(() => {
-        async function main() {
-        // Executes the counter function to query the current contract state
-        // the `.get()` is read-only, because of this it don't expand coins.
-        const { value } = await contract.functions.is_registered().txParams({ gasPrice: 1 }).call();
-        console.log(value);
-        // const { value } = await contract.functions.is_registered().get();
-        // console.log(value);
-        // setRegistered(value);
-        }
-        main();
-    }, []);
+    // useEffect(() => {
+    //     async function main() {
+    //     // Executes the counter function to query the current contract state
+    //     // the `.get()` is read-only, because of this it don't expand coins.
+    //     const { value } = await contract.functions.is_registered().txParams({ gasPrice: 100 }).call();
+    //     // console.log(value);
+    //     // const { value } = await contract.functions.is_registered().get();
+    //     // console.log(value);
+    //     setRegistered(value);
+    //     }
+    //     main();
+    // }, []);
 
     // register user
   async function register() {
@@ -39,9 +46,11 @@ const MessageBox = (wallet:  WalletUnlocked, contract: Contract) => {
       // await contract.functions.register(MSG_PK).txParams({ gasPrice: 10 }).call();
       // await contract.functions.add_contact(wallet.address.toHexString()).txParams({ gasPrice: 10 }).call();
       // await contract.functions.is_registered().txParams({ gasPrice: 1 }).call();
-      const { value } = await contract.functions.is_registered().txParams({ gasPrice: 10 }).call();
+    //   const { value } = await contract.functions.is_registered().txParams({ gasPrice: 1000}).call();
       // await contract.functions.add_contact(wallet.address.toHexString()).txParams({ gasPrice: 10 }).call();
-      await contract.functions.message(wallet.address.toHexString(), "HELLO").txParams({ gasPrice: 10 }).call();
+      await contract.functions.message(wallet.address.toHexString(), "HELLOabc").txParams({ gasPrice: 1000 }).call();
+
+      const { value } = await contract.functions.is_registered().txParams({ gasPrice: 1000}).call();
       // console.log(value);
       setRegistered(value);
     } finally {
